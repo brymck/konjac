a4e469e37fb3a51fe1b838fd21f668c20cd6f6b9
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-01-13 00:25:41 +0900

    Super-awesome way of getting rid of redundant tags in word for a cleaner tags file

--- lib/konjac/word.rb
+++ lib/konjac/word.rb
@@ -46,26 +46,108 @@ module Konjac
           sub_files = Dir.glob(File.expand_path(file))
           sub_files.each do |sub_file|
             # Build a list of all the paths we're working with
-            dirname   = File.dirname(sub_file)
-            basename  = File.basename(sub_file, ".*")
-            xml_path  = "#{dirname}/#{basename}.xml"
-            tags_path = "#{dirname}/#{basename}.tags"
+            dirname    = File.dirname(sub_file)
+            basename   = File.basename(sub_file, ".*")
+            xml_path   = "#{dirname}/#{basename}_orig.xml"
+            clean_path = "#{dirname}/#{basename}.xml"
+            tags_path  = "#{dirname}/#{basename}.tags"
 
             # Unzip the DOCX's word/document.xml file and pipe the output into
             # an XML with the same base name as the DOCX
             system "unzip -p #{sub_file} word/document.xml > #{xml_path}"
 
             # Read in the XML file and extract the content from each <w:t> tag
-            reader = Nokogiri::XML(File.read(xml_path))
+            cleaner = Nokogiri::XML(File.read(xml_path))
             File.open(tags_path, "w") do |tags_file|
-              reader.xpath("//w:t").each do |node|
+              # Remove all grammar and spellcheck tags
+              cleaner.xpath("//w:proofErr").remove
+
+              nodes = cleaner.xpath("//w:r")
+              prev = nil
+              nodes.each do |node|
+                unless prev.nil?
+                  if (prev.next_sibling == node) && compare_nodes(prev, node)
+                    begin
+                      node.at_xpath("w:t").content = prev.at_xpath("w:t").content +
+                        node.at_xpath("w:t").content
+                      prev.remove
+                    rescue
+                    end
+                  end
+                end
+                
+                prev = node
+              end
+
+              cleaner.xpath("//w:t").each do |node|
                 tags_file.puts node.content
               end
+            end
+
+            File.open(clean_path, "w") do |xml|
+              xml.puts cleaner.to_xml
             end
           end
         end
       end
+
+      private
+
+      # Performs a comparison between two nodes and accepts them as equivalent
+      # if the differences are very minor
+      def compare_nodes(a, b)
+        c = clean_hash(xml_node_to_hash(a))
+        d = clean_hash(xml_node_to_hash(b))
+        c == d
+      end
+
+      def xml_node_to_hash(node)
+        # If we are at the root of the document, start the hash
+        if node.element?
+          result_hash = {}
+          if node.attributes != {}
+            result_hash[:attributes] = {}
+            node.attributes.keys.each do |key|
+              result_hash[:attributes][node.attributes[key].name.to_sym] = prepare(node.attributes[key].value)
+            end
+          end
+          if node.children.size > 0
+            node.children.each do |child|
+              result = xml_node_to_hash(child)
+
+              if child.name == "text"
+                unless child.next_sibling || child.previous_sibling
+                  return prepare(result)
+                end
+              elsif result_hash[child.name.to_sym]
+                if result_hash[child.name.to_sym].is_a?(Array)
+                  result_hash[child.name.to_sym] << prepare(result)
+                else
+                  result_hash[child.name.to_sym] = [result_hash[child.name.to_sym]] << prepare(result)
+                end
+              else
+                result_hash[child.name.to_sym] = prepare(result)
+              end
+            end
+
+            return result_hash
+          else
+            return result_hash
+          end
+        else
+          return prepare(node.content.to_s)
+        end
+      end
+
+      def prepare(data)
+        (data.class == String && data.to_i.to_s == data) ? data.to_i : data
+      end
+
+      # Delete extraneous attributes for comparison
+      def clean_hash(hash)
+        hash.delete :t
+        hash[:rPr][:rFonts][:attributes].delete :hint
+      end
     end
   end
 end
-# lol
