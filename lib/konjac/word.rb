3c827c1204ca181d930fa55495bb4da8c6d0d6ce
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-01-12 22:33:38 +0900

    Casts Beta

--- lib/konjac/word.rb
+++ lib/konjac/word.rb
@@ -6,19 +6,36 @@ module Konjac
         files.each do |file|
           sub_files = Dir.glob(File.expand_path(file))
           sub_files.each do |sub_file|
+            # Build the list of paths we need to work with
             dirname   = File.dirname(sub_file)
             basename  = File.basename(sub_file, ".*")
+            new_path  = "#{dirname}/#{basename}_imported.docx"
             xml_path  = "#{dirname}/#{basename}.xml"
             tags_path = "#{dirname}/#{basename}.tags"
+            out_path  = "#{dirname}/word/document.xml"
 
-            reader = Nokogiri::XML::Reader(File.read(xml_path))
-            File.open(tags_path, "w") do |tags_file|
-              reader.each do |node|
-                if node.name == "#text"
-                  tags_file.puts node.value
-                end
-              end
+            # Open the original XML file and the updated tags
+            writer = Nokogiri::XML(File.read(xml_path))
+            tags   = File.readlines(tags_path)
+
+            # Overwrite each <w:t> tag's content with the new tag
+            writer.xpath("//w:t").each do |node|
+              node.content = tags.shift.tr("\n", "")
             end
+
+            # Create a directory for word/document.xml if necessary
+            FileUtils.mkdir "#{dirname}/word" unless File.directory?("#{dirname}/word")
+
+            # Write the modified XML to a file
+            File.open(out_path, "w") do |file|
+              file.write writer.to_xml.gsub(/\n\s*/, "").sub(/\?></, "?>\n<")
+            end
+
+            # Copy the original file
+            FileUtils.cp sub_file, new_path
+
+            # Add the new document XML to the copied file
+            system "cd #{dirname} && zip -q #{new_path} word/document.xml"
           end
         end
       end
@@ -28,19 +45,21 @@ module Konjac
         files.each do |file|
           sub_files = Dir.glob(File.expand_path(file))
           sub_files.each do |sub_file|
+            # Build a list of all the paths we're working with
             dirname   = File.dirname(sub_file)
             basename  = File.basename(sub_file, ".*")
             xml_path  = "#{dirname}/#{basename}.xml"
             tags_path = "#{dirname}/#{basename}.tags"
 
+            # Unzip the DOCX's word/document.xml file and pipe the output into
+            # an XML with the same base name as the DOCX
             system "unzip -p #{sub_file} word/document.xml > #{xml_path}"
 
-            reader = Nokogiri::XML::Reader(File.read(xml_path))
+            # Read in the XML file and extract the content from each <w:t> tag
+            reader = Nokogiri::XML(File.read(xml_path))
             File.open(tags_path, "w") do |tags_file|
-              reader.each do |node|
-                if node.name == "#text"
-                  tags_file.puts node.value
-                end
+              reader.xpath("//w:t").each do |node|
+                tags_file.puts node.content
               end
             end
           end
