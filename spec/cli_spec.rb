22e337fabf8d51da4c3ca41220e81dc2f50351d3
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-01-13 15:04:50 +0900

    Handle warnings from changing ARGV

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,8 +3,15 @@ require File.dirname(__FILE__) + "/spec_helper"
 require "tempfile"
 
 describe CLI do
+  # Super hacky way of setting a constant without setting off a warnings that the
+  # constant has already been initialized
+  def set_argv(*params)
+    Object.__send__ :remove_const, :ARGV
+    Object.const_set :ARGV, params
+  end
+
   it "should fail on an invalid subcommand" do
-    ARGV = ["invalid"]
+    set_argv "invalid"
     lambda { CLI.start }.should raise_error InvalidCommandError
   end
 
@@ -14,8 +21,8 @@ describe CLI do
       @dictionary = Tempfile.new(["dict", ".yml"])
       @dictionary.write <<-eos
                           -
-                           en: dogs
-                           ja: 犬
+                            en: dogs
+                            ja: 犬
                         eos
       @dictionary.rewind
 
@@ -23,20 +30,27 @@ describe CLI do
       @english = Tempfile.new(["english", "_en.txt"])
       @english.write "I like dogs."
       @english.rewind
+
+      # Set ARGV
+      set_argv "translate", @english.path, "into", "japanese", "using",
+        @dictionary.path
     end
 
     it "should correctly translate English text" do
       begin
-        ARGV = ["translate", @english.path, "into", "japanese", "using", @dictionary.path]
         CLI.start
         converted_path = Utils.build_converted_file_name(@english.path, "en", "ja")
-        puts File.read(@english.path).should == "I like dogs."
+        File.read(@english.path).should == "I like dogs."
         File.read(converted_path).should == "I like 犬.\n"
       ensure
         @dictionary.close!
         @english.close!
         File.delete converted_path
       end
+    end
+
+    it "should return the name of the file to translate" do
+      CLI.start.should == [@english.path]
     end
   end
 end
