60e4448b119bcb4f5642741e908f4cf0f08d5f19
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-02-12 16:53:39 +0900

    Added tags to Office program tests

---
+++ spec/office/word_spec.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Office::Mac::Word, :word do
+  it "should not be raised" do
+    1.should == 2
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 require "konjac"
 include Konjac
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
